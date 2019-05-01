b35d8b5414fdbf26ff663fcc76c0bef1b124b08b
Christian Valentin <chris_valentin@outlook.com>
2019-05-01 02:08:27 -0300

    Updates

---
+++ precache-manifest.c1c8ba7dd763b8d0c39838eb3fa33d07.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27b82d66e1dc028e8214cfac524b7ed5",
+    "url": "/music-db/index.html"
+  },
+  {
+    "revision": "e1e89da6848c7433c22c",
+    "url": "/music-db/static/css/main.c1338f3b.chunk.css"
+  },
+  {
+    "revision": "924c195bb82db78a0554",
+    "url": "/music-db/static/js/2.bc098a12.chunk.js"
+  },
+  {
+    "revision": "e1e89da6848c7433c22c",
+    "url": "/music-db/static/js/main.7b53b52b.chunk.js"
+  },
+  {
+    "revision": "80c5619abab92be437a6",
+    "url": "/music-db/static/js/runtime~main.5e07afb3.js"
+  },
+  {
+    "revision": "45ad0cac245cd460c811ab3636f8a8fa",
+    "url": "/music-db/static/media/404-background.45ad0cac.jpg"
+  },
+  {
+    "revision": "9343fe58cc158b49fdb4e9ce79e017de",
+    "url": "/music-db/static/media/logo.9343fe58.png"
+  },
+  {
+    "revision": "57cd97e6b312036d3dfd28d1f2326eee",
+    "url": "/music-db/static/media/thumb-placeholder.57cd97e6.png"
+  }
+]);
