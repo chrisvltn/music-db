3b3a0b86a79b2f44d6cb243576ea9355d8d5d76c
chrisvltn <chris_valentin@outlook.com>
2019-05-01 17:17:04 -0300

    Adds default value parameter to withShow HOC

--- src/hoc/withShow/withShow.tsx
+++ src/hoc/withShow/withShow.tsx
@@ -8,8 +8,8 @@ interface WithShowProps {
  * Adds `show` property to set if the component is being showed or not
  * @param WrappedComponent
  */
-const withShow = <P extends object>(WrappedComponent: React.ComponentType<P>): React.FC<P & WithShowProps> =>
-	({ show, ...props }) =>
+const withShow = <P extends object>(WrappedComponent: React.ComponentType<P>, defaultValue: boolean = true): React.FC<P & WithShowProps> =>
+	({ show = defaultValue, ...props }) =>
 		show ? <WrappedComponent {...props as P} /> : null
 
 export default withShow
